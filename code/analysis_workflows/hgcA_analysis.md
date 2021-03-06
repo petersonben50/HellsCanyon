9af7255682d22e9d47b1396c79d7378b420af26a
Benjamin D. Peterson <petersonben50@gmail.com>
2021-03-05 21:26:40 -0600

    Added hgcA processing scripts

---
+++ code/analysis_workflows/hgcA_analysis.md
@@ -0,0 +1,124 @@
+
+
+
+
+**Identify hgcA sequences**
+
+*Identify putative hgcA genes with HMM*
+
+I used the HMM I built for the 5M project to search through the open reading frames from the assemblies for HgcA-like sequences.
+These results were stored in folders by year.
+I then pulled out the amino acid sequences for each of the putative HgcA sequences.
+
+*Generate key for year of analysis*
+
+
+
+*Concatenate and align all hgcA seqs for curation*
+
+I aligned all the identified hgcA sequences to the HMM using hmmalign, then converted the file to the fasta format.
+I downloaded this file (`dataEdited/hgcA_analysis/hgcA_raw.afa`) and loaded it into Geneious.
+Here are my notes on the curation of those sequences:
+
+- fall2017cluster5_000000264767_1 is truncated at the C-terminus. Let's hang on to it for now. All seqs truncated like this will be added to this list: `dataEdited/hgcA_analysis/identification/truncated_hgcA_seq.txt`
+- fall2017cluster6_000000245170_1 also is truncated. Also, let's keep this one around for now.
+- fall2017cluster6_000000208658_3 doesn't have the cap helix domain. Cut it!
+- fall2017coassembly_000001368876_2 truncated at C-terminus.
+- fall2017coassembly_000000596207_3 truncated at C-terminus.
+- fall2017coassembly_000000643563_1 truncated at C-terminus.
+- fall2017coassembly_000000746684_1 missing cap helix domain
+- fall2017coassembly_000000746684_1 truncated at N-terminus
+- fall2017coassembly_000001272590_2 truncated at C-terminus, very close to cap helix domain.
+- HC18HY300_000000198900_1 truncated at C-terminus, very close to cap helix domain.
+- HC18HY300_000000087507_1 truncated at C-terminus, very close to cap helix domain.
+- KMBP004E_000000161924_1 truncated at C-terminus, very close to cap helix domain.
+- KMBP004F_000000165420_1 truncated at C-terminus
+- KMBP004F_000000644814_3 truncated at N-terminus
+- KMBP004F_000000253505_6 truncated at N-terminus
+
+Exported `hgcA_good.afa` and uploaded it to the GLBRC.
+
+
+**Pull out depth of hgcA+ scaffolds**
+
+I then extracted the depths of all the hgcA+ scaffolds from the metagenomes.
+I used samtools for this.
+When aggregating the depth over a scaffold, I took the mean of the coverage, not including the 150 bp at either end of the scaffold.
+Wonder if I should calculate the median instead?
+
+**Genomic context for hgcA**
+
+Next I pulled out the hgcA+ scaffolds and the corresponding GFF files.
+
+*Search downstream genes for hgcB*
+
+I pulled out the gene immediately downstream from ecah *hgcA* using `retrieve_downstream_gene_name.py`.
+I ran those ORFs against the *hgcB* HMM I built for the 5M project.
+I aligned the hits against the HMM and checked them in Geneious.
+These all look good for the most part. fall2017cluster4_000000001920_1 and HC18HY300_000000119163_1 are a little truncated, but the motif we need to see is there (or at least mostly).
+We'll stick with the output we have there.
+
+I then manually checked the seven non-hgcB sequences using [MOTIF](https://www.genome.jp/tools/motif/):
+  - fall2017cluster6_000000000428_49 - PF02627 is the only hit. Carboxymuconolactone decarboxylase. Not a great hit.
+  - fall2017coassembly_000000448578_3: No motif found. Checked it on blast: Closest was hypothetical protein Bacteroidales bacterium.
+  - fall2017coassembly_000001334838_1: Voltage-dependent anion channel, super low support for this. Probably hypothetical.
+  - HC18HY300_000000013755_6 - this matches to a sodium bile symporter... weird. Pretty good match too, e-value of 5.9e-51. Very weird
+  - HC18ME02_000000018262_3 - This one had the identical sequence to the one above. Must be the same sequence, just assembled two different times.
+  - KMBP004F_000000216801_1: Nothing with high support. BLAST: hypothetical. Closest hit is 37% identity.
+  - KMBP009B_000000084934_2: transposase nuclease. Interesting. Wonder if this is a transposon?
+
+*Isolate gene neighborhoods*
+
+I extracted the region 5000 bp on either side of the hgcA sequences.
+I'll want to look at this later when I'm getting the details on the genomic context for *hgcA*.
+
+*Manually inspect scaffold for hgcB*
+
+In the `hgcA_data_aggregation.R` file, I read out a csv file (`hgcB_notes.csv`) with the *hgcA* and *hgcB* sequences linked by their scaffold IDs.
+I saved this out as an xlsx file and added a notes column with notes from my manual inspection of the gene neighborhoods.
+
+
+**Classify hgcA seqs with pplacer workflow**
+
+I used a workflow developed by Caitlin Gionfriddo to classify the *hgcA* genes, so all credit to her for this really nice workflow.
+A very helpful tutorial can be found here: https://caitlingio.com/tutorial-for-hgcab-amplicon-sequencing-data/.
+It relies on the Hg-MATE database (I used version 1 for this analysis).
+First is the generation of an alignment of the HgcA sequences from this study using MUSCLE.
+Then, consensus alignment is made between this alignment and the alignment of the Hg-MATE database.
+The sequences are placed on a pre-generated tree using pplacer.
+The program rppr is used to make a sqlite database of the taxonomy of the reference sequences.
+Guppy was then used to classify the sequences and visualize the placements on a tree.
+Finally, I used the R script `clean_hgcA_classification.R` (also adapted from Caitlin's well-annotated workflow) to clean up the classification and save out a csv file with that information.
+
+
+**Dereplicate sequences**
+
+Okay, did an initial dereplication with CD-HIT with all the sequences, to see if there was any cross-over across different years.
+There is... now how do I account for this?
+
+I am including all the *hgcA* sequences in the final hgcA table, with a column for overall representative and another key with the year that that sequence will be counted in.
+Essentially, I picked a representative from each cluster overall and one for each year.
+I aggregated all the data here: `hgcA_data_aggregation.R`.
+Save out a csv here: `dataEdited/hgcA_analysis/hgcA_dereplication_data.csv`.
+Save this as an Excel file `hgcA_dereplication.xlsx`.
+Add a column (representative) and assign a TRUE or FALSE depending if this gene is to be a representative of its cluster.
+Also add a column (usedForAbundance) to filter out the sequences that will be used for abundance calculations.
+These two columns will be similar, except that the usedForAbundance column will include multiples of some clusters, for clusters that span multiple years.
+I am not including sequences that are truncated in the abundance calculations though (i.e., cluster 13 has an *hgcA* from 2017 and 2018 samples, but the 2017 sample is truncated).
+Also not including the truncated samples in the tree.
+
+
+**Phylogenetic analysis of hgcA**
+
+Lastly, I wanted to generated a phylogenetic tree with representatives from each of the HgcA clusters.
+I first pulled out the sequences that I determined I would use in the tree (1 from each cluster).
+I then aligned these sequences to each other, then generated a consensus alignment with the Hg-MATE reference database.
+I then used FastTree to generate a rough HgcA tree.
+I looked at this tree in R (`clean_hgcA_tree.R`).
+I first looked at the unrooted tree to find the branch leading to the paralogs, then rooted the tree there.
+I read out a pdf of the tree and identified nodes that led to tips that were not needed.
+I removed all the sequences under these nodes and read out a list of the sequence names that I wanted to include.
+I then used a custom script to pull out the sequences that I didn't want to include.
+I masked the alignment at 50% gaps in Geneious.
+I then used this alignment to generate a maximum-likelihood tree using RAxML.
+There were a few duplicated sequences, so I used the `.reduced` file that RAxML generated.
---
+++ code/analysis_workflows/hgcA_analysis.sh
@@ -0,0 +1,438 @@
+#!/bin/sh
+
+######################
+# code/hgcA_analysis/hgcA_analysis.sh
+# Benjamin D. Peterson
+
+# This set of scripts will contain the bash
+# code we need to pull out the hgcA sequences
+# from our assembly and process them.
+######################
+
+
+############################################
+############################################
+# Identify hgcA sequences
+############################################
+############################################
+
+######################
+# Identify putative hgcA genes with HMM
+######################
+
+screen -S HCC_hgcA_search
+source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
+conda activate bioinformatics
+PYTHONPATH=''
+PERL5LIB=''
+scripts=~/HellsCanyon/code/generalUse/
+ORFs=~/HellsCanyon/dataEdited/assemblies/ORFs
+mkdir ~/HellsCanyon/dataEdited/hgcA_analysis
+mkdir ~/HellsCanyon/dataEdited/hgcA_analysis/identification
+cd ~/HellsCanyon/dataEdited/hgcA_analysis/identification
+mkdir 2017 2018 2019
+cd ~/HellsCanyon/dataEdited
+
+cat ~/HellsCanyon/metadata/lists/assembly_key.csv | while read -r line
+do
+  assembly=`echo $line | awk -F ',' '{ print $1 }'`
+  year=`echo $line | awk -F ',' '{ print $2 }'`
+  echo "Searching for hgcA in" $assembly "from" $year
+  if [ -e $ORFs/$assembly.faa ]; then
+    if [ ! -e hgcA_analysis/identification/$year/$assembly\_hgcA_report.txt ]; then
+      echo "Search for hgcA in" $assembly
+      hmmsearch --tblout hgcA_analysis/identification/$year/$assembly\_hgcA.out \
+                --cpu 4 \
+                --cut_tc \
+                ~/references/hgcA/hgcA.hmm \
+                $ORFs/$assembly.faa \
+                > hgcA_analysis/identification/$year/$assembly\_hgcA_report.txt
+      python $scripts/extract_protein_hitting_HMM.py \
+              hgcA_analysis/identification/$year/$assembly\_hgcA.out \
+              $ORFs/$assembly.faa \
+              hgcA_analysis/identification/$year/$assembly\_hgcA.faa
+    else
+      echo "Search is already done in" $assembly
+    fi
+  else
+    echo "Genes aren't predicted for" $assembly
+  fi
+done
+
+
+######################
+# Concatenate and align all hgcA seqs for curation
+######################
+cd ~/HellsCanyon/dataEdited/hgcA_analysis/identification/
+cat 201*/*_hgcA.faa > hgcA_raw.faa
+# Align seqs
+hmmalign -o hgcA_raw.sto \
+            ~/references/hgcA/hgcA.hmm \
+            hgcA_raw.faa
+$scripts/convert_stockhold_to_fasta.py hgcA_raw.sto
+
+# Curate the hgcA list, see notes in md file.
+# Then:
+cd ~/HellsCanyon/dataEdited/hgcA_analysis/identification/
+grep '>' hgcA_good.afa | \
+    sed 's/>//' \
+    > hgcA_good.txt
+sed 's/-//g' hgcA_good.afa > hgcA_good.faa
+
+
+############################################
+############################################
+# Pull out depth of hgcA+ scaffolds
+############################################
+############################################
+
+screen -S HCC_hgcA_depth
+cd ~/HellsCanyon/dataEdited/hgcA_analysis
+mkdir depth
+source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
+conda activate bioinformatics
+PERL5LIB=""
+PYTHONPATH=""
+
+awk -F ',' '{ print $1 }' ~/HellsCanyon/metadata/lists/metagenome_key.csv | while read metagenome
+do
+  cat identification/hgcA_good.txt | while read gene
+  do
+    scaffold=$(echo $gene | awk -F '_' '{ print $1"_"$2 }')
+    assembly=$(echo $gene | awk -F '_' '{ print $1 }')
+    if [ -e ~/HellsCanyon/dataEdited/mapping/$metagenome\_to_$assembly.bam ]; then
+      echo "Calculating coverage of" $metagenome "over" $scaffold
+      samtools depth -a -r $scaffold ~/HellsCanyon/dataEdited/mapping/$metagenome\_to_$assembly.bam \
+          >> depth/$metagenome\_hgcA_depth_raw.tsv
+    else
+      echo $metagenome "not from same year as" $assembly "and" $gene "won't be found there"
+    fi
+  done
+
+  echo "Aggregating hgcA depth information for" $metagenome
+  python ~/HellsCanyon/code/generalUse/calculate_depth_length_contigs.py \
+            depth/$metagenome\_hgcA_depth_raw.tsv \
+            150 \
+            depth/$metagenome\_hgcA_depth.tsv
+  rm -f depth/$metagenome\_hgcA_depth_raw.tsv
+done
+
+
+
+############################################
+############################################
+# Genomic context for hgcA
+############################################
+############################################
+
+######################
+# First pull out hgcA+ scaffolds
+######################
+screen -S HCC_hgcA_context
+cd ~/HellsCanyon/dataEdited/hgcA_analysis
+# rm -r scaffolds
+mkdir scaffolds
+
+# Pull out scaffold FNA files
+grep '>' identification/hgcA_raw.faa | \
+    sed 's/>//' | \
+    awk -F '_' '{ print $1"_"$2 }' \
+    > scaffolds/hgcA_raw_scaffold_list.txt
+rm scaffolds/hgcA_scaffolds.fna
+cat scaffolds/hgcA_raw_scaffold_list.txt | while read scaffold
+do
+  assemblyName=$(echo $scaffold | awk -F '_' '{ print $1 }')
+  echo "Pulling out" $scaffold "from" $assemblyName
+  grep -A 1 $scaffold\$ ~/HellsCanyon/dataEdited/assemblies/scaffolds/$assemblyName\_assembly.fna \
+      >> scaffolds/hgcA_scaffolds.fna
+done
+
+# Pull out GFF entries
+rm -f scaffolds/hgcA_scaffolds.gff
+cat scaffolds/hgcA_raw_scaffold_list.txt | while read scaffold
+do
+  assemblyName=$(echo $scaffold | awk -F '_' '{ print $1 }')
+  echo "Pulling out" $scaffold "GFF entries"
+  awk -v scaffold="$scaffold" '{ if ($1 == scaffold) print }' ~/HellsCanyon/dataEdited/assemblies/ORFs/$assemblyName.gff \
+      >> scaffolds/hgcA_scaffolds.gff
+done
+
+
+
+######################
+# Search downstream genes for hgcB
+######################
+source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
+conda activate py_viz
+PYTHONPATH=''
+IFS=$'\n'
+scripts=~/HellsCanyon/code/generalUse
+ORFs=~/HellsCanyon/dataEdited/assemblies/ORFs
+cd ~/HellsCanyon/dataEdited/hgcA_analysis
+mkdir hgcB
+rm -f hgcB/downstream_gene_list.txt
+python $scripts/retrieve_downstream_gene_name.py \
+          identification/hgcA_good.txt \
+          scaffolds/hgcA_scaffolds.gff \
+          hgcB/downstream_gene_list.txt
+
+# Extract downstream amino acid sequences
+rm -f hgcB/downstream_genes.faa
+cat hgcB/downstream_gene_list.txt | while read gene
+do
+  assemblyName=$(echo $gene | cut -d "_" -f1)
+  echo "Pulling out" $gene "faa entries"
+  grep -A 1 $gene$ $ORFs/$assemblyName.faa >> hgcB/downstream_genes.faa
+done
+conda deactivate
+
+
+# Search adjacent genes with hgcB HMM
+cd ~/HellsCanyon/dataEdited/hgcA_analysis
+source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
+conda activate bioinformatics
+PYTHONPATH=''
+PERL5LIB=''
+scripts=~/HellsCanyon/code/generalUse
+hmmsearch --tblout hgcB/hgcB.out \
+          --cpu 4 \
+          -T 30 \
+          ~/references/hgcA/hgcB_5M.HMM \
+          hgcB/downstream_genes.faa \
+          > hgcB/hgcB_report.txt
+
+# Check the hgcB hits
+rm -f hgcB/hgcB.faa
+grep -v "#" hgcB/hgcB.out | awk '{ print $1 }' | while read geneID
+do
+  grep -A 1 $geneID$ hgcB/downstream_genes.faa >> hgcB/hgcB.faa
+done
+grep '>' hgcB/hgcB.faa | wc -l
+# Align sequences to HMM
+hmmalign -o hgcB/hgcB.sto \
+            ~/references/hgcA/hgcB_5M.HMM \
+            hgcB/hgcB.faa
+# Convert alignment to fasta format
+$scripts/convert_stockhold_to_fasta.py hgcB/hgcB.sto
+
+
+# Check sequences in Geneious.
+# They all check out, so I'll just use the hgcB.faa
+# file as our final set of hgcB seqs.
+cd ~/HellsCanyon/dataEdited/hgcA_analysis/hgcB
+grep '>' hgcB.faa | \
+  sed 's/>//' > hgcB.txt
+
+# Pull out non-hgcB downstream genes
+cd ~/HellsCanyon/dataEdited/hgcA_analysis/hgcB
+cat downstream_gene_list.txt hgcB.txt | \
+  sort | \
+  uniq -u
+grep -A 1 'fall2017cluster6_000000000428_49' downstream_genes.faa
+grep -A 1 'fall2017coassembly_000000448578_3' downstream_genes.faa
+grep -A 1 'fall2017coassembly_000001334838_1' downstream_genes.faa
+grep -A 1 'HC18HY300_000000013755_6' downstream_genes.faa
+grep -A 1 'HC18ME02_000000018262_3' downstream_genes.faa
+grep -A 1 'KMBP004F_000000216801_1' downstream_genes.faa
+grep -A 1 'KMBP009B_000000084934_2' downstream_genes.faa
+
+
+
+
+######################
+# Isolate gene neighborhoods
+######################
+
+screen -S HCC_hgcA_gene_neighborhood
+cd ~/HellsCanyon/dataEdited/hgcA_analysis
+source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
+conda activate py_viz
+PYTHONPATH=''
+IFS=$'\n'
+scripts=~/HellsCanyon/code/generalUse
+
+grep '>' identification/hgcA_raw.faa | \
+    sed 's/>//' | \
+    while read hgcA_id
+    do
+      echo "Working on" $hgcA_id
+      scaffold_id=$(echo $hgcA_id | cut -d '_' -f 1-2)
+      awk -F '\t' -v scaffold_id="$scaffold_id" '$1 == scaffold_id { print $0 }' scaffolds/hgcA_scaffolds.gff > scaffolds/temp_scaffolds.gff
+      gene_id=$(echo $hgcA_id | \
+                  cut -d '_' -f 2-3 | \
+                  sed 's/^0*//g')
+      echo "Searching for" $gene_id
+      python $scripts/gene_neighborhood_extraction.py scaffolds/temp_scaffolds.gff \
+                                                      scaffolds/hgcA_scaffolds.fna \
+                                                      $gene_id \
+                                                      5000 \
+                                                      scaffolds/temp_$gene_id
+
+      rm -f scaffolds/temp_scaffolds.gff
+    done
+
+cd scaffolds
+rm -f hgcA_geneNeighborhood.gff hgcA_geneNeighborhood.fna
+cat temp_*.gff > hgcA_geneNeighborhood_raw.gff
+cat temp_*.fna > hgcA_geneNeighborhood_raw.fna
+rm -f *_neighborhood.*
+
+
+############################################
+############################################
+# Classify hgcA seqs with pplacer workflow
+############################################
+############################################
+
+screen -S HCC_hgcA_pplacer
+source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
+conda activate bioinformatics
+PYTHONPATH=""
+PERL5LIB=""
+references=~/HellsCanyon/references/hgcA
+workingDirectory=~/HellsCanyon/dataEdited/hgcA_analysis
+scripts=~/HellsCanyon/code/generalUse
+mkdir $workingDirectory/classification
+
+# Generate fasta file of reference alignment
+cp -avr ~/Everglades/references/hgcA/Hg-MATE-Db.v1.ISOCELMAG_HgcA_full.refpkg $references/
+cd $references/Hg-MATE-Db.v1.ISOCELMAG_HgcA_full.refpkg
+$scripts/convert_stockhold_to_fasta.py Hg-MATE-Db.v1.ISOCELMAG_HgcA_full.stockholm
+mv Hg-MATE-Db.v1.ISOCELMAG_HgcA_full.afackholm $workingDirectory/classification/Hg-MATE-Db.v1.ISOCELMAG_HgcA_full.afa
+
+# Generate alignment of sequences of interest
+cd $workingDirectory
+muscle -in identification/hgcA_good.faa \
+        -out classification/hgcA_muscle.afa
+cd classification/
+
+# Combine the alignments of seqs from this study and references
+muscle -profile -in1 hgcA_muscle.afa \
+        -in2 Hg-MATE-Db.v1.ISOCELMAG_HgcA_full.afa \
+        -out hgcA_for_classification.afa
+# Convert to stockholm format
+python $scripts/convert_fasta_to_stockholm.py hgcA_for_classification.afa
+conda deactivate
+
+# Run pplacer
+conda activate hgcA_classifier
+pplacer -p \
+        --keep-at-most 1 \
+        --max-pend 1 \
+        -c $references/Hg-MATE-Db.v1.ISOCELMAG_HgcA_full.refpkg/ \
+        hgcA_for_classification.sto
+
+# Make sqlite database of reference
+rppr prep_db \
+      --sqlite Hg_MATE_classify \
+      -c $references/Hg-MATE-Db.v1.ISOCELMAG_HgcA_full.refpkg
+
+# Generate taxonomic assignments using guppy
+guppy classify -c $references/Hg-MATE-Db.v1.ISOCELMAG_HgcA_full.refpkg \
+               --pp \
+               --sqlite Hg_MATE_classify \
+               hgcA_for_classification.jplace
+
+# Save out this data to csv
+guppy to_csv --point-mass \
+              --pp \
+              -o hgcA_classification.csv \
+              hgcA_for_classification.jplace
+
+# Visualize placements on FastTree
+guppy tog --pp \
+          -o hgcA_classification.nwk \
+          hgcA_for_classification.jplace
+
+
+
+
+
+############################################
+############################################
+# hgcA dereplication
+############################################
+############################################
+
+cd ~/HellsCanyon/dataEdited/hgcA_analysis
+mkdir dereplication
+cdhit=~/programs/cdhit-master
+$cdhit/cd-hit -g 1 \
+              -i identification/hgcA_good.faa \
+              -o dereplication/hgcA_good_acrossYear.faa \
+              -c 0.97 \
+              -n 5 \
+              -d 0
+$cdhit/clstr2txt.pl dereplication/hgcA_good_acrossYear.faa.clstr \
+  > dereplication/hgcA_good_acrossYear.tsv
+
+
+
+############################################
+############################################
+# Phylogenetic analysis of hgcA
+############################################
+############################################
+screen -S HCC_hgcA_tree
+cd ~/HellsCanyon/dataEdited/hgcA_analysis
+workingDirectory=~/HellsCanyon/dataEdited/hgcA_analysis/phylogeny
+scripts=~/HellsCanyon/code/generalUse
+references=~/HellsCanyon/references/hgcA
+mkdir $workingDirectory
+
+# Pull out sequences for phylogenetic analysis.
+# Add hgcA_rep_list.txt to the phylogeny folder
+cd ~/HellsCanyon/dataEdited/hgcA_analysis/
+rm -f phylogeny/hgcA_for_phylogeny.faa
+cat phylogeny/hgcA_rep_list.txt | while read hgcA
+do
+  echo "Including" $hgcA "in phylogenetic analysis"
+  grep -A 1 $hgcA identification/hgcA_good.faa >> phylogeny/hgcA_for_phylogeny.faa
+done
+
+# Generate alignment
+cd $workingDirectory
+source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
+conda activate bioinformatics
+PYTHONPATH=""
+PERL5LIB=''
+muscle -in hgcA_for_phylogeny.faa \
+        -out hgcA_for_phylogeny.afa
+muscle -profile \
+        -in1 hgcA_for_phylogeny.afa \
+        -in2 $references/Hg-MATE-Db.v1.ISOCELMAG_HgcA_full.refpkg/Hg-MATE-Db.v1.ISOCELMAG_HgcA_full_align-bmge.fasta \
+        -out hgcA_for_phylogeny_raw.afa
+
+# Generate rough tree
+FastTree hgcA_for_phylogeny_raw.afa \
+    > rough_hgcA.tree
+      # Download this to my local computer.
+
+# Upload the list of sequences to remove
+python $scripts/remove_fasta_seqs_using_list_of_headers.py hgcA_for_phylogeny_raw.afa \
+                                                            seqs_to_remove.txt \
+                                                            hgcA_for_phylogeny.afa
+
+#########################
+# Generate tree in RAxML
+#########################
+
+screen -S HCC_hgcA_tree
+cd ~/HellsCanyon/dataEdited/hgcA_analysis/phylogeny
+raxml=/opt/bifxapps/raxml-8.2.11/raxmlHPC-PTHREADS
+#$raxml -f a \
+#        -p 283976 \
+#        -m PROTGAMMAAUTO \
+#        -N autoMRE \
+#        -x 2381 \
+#        -T 20 \
+#        -s hgcA_for_phylogeny_masked.afa \
+#        -n hgcA
+$raxml -f a \
+        -p 283976 \
+        -m PROTGAMMAAUTO \
+        -N autoMRE \
+        -x 2381 \
+        -T 20 \
+        -s hgcA_for_phylogeny_masked.afa.reduced \
+        -n hgcA
