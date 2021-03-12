89b4fe114a045fb80546642fd0e3400813c9c231
Benjamin D. Peterson <petersonben50@gmail.com>
2021-03-12 09:11:46 -0600

    Aggregating metabolic gene depth

---
+++ code/metabolic_analyses/metabolic_proteins_depth_aggregate.R
@@ -0,0 +1,47 @@
+#### code/metabolic_analyses/metabolic_proteins_depth_aggregate.R ####
+# Benjamin D. Peterson
+
+
+#### Clean up crew on line 5 ####
+
+rm(list = ls())
+setwd("/Users/benjaminpeterson/Documents/research/HellsCanyon")
+library(tidyverse)
+
+
+#### Read in normalization vector ####
+normalized.coverage.vector <- readRDS("dataEdited/scg_abundance/scg_normalization_vector.rds")
+
+
+#### Read in depth data ####
+list.o.depths <- list.files(path = "dataEdited/metabolic_analyses/depth",
+                            pattern = "depth.tsv",
+                            full.names = TRUE)
+
+raw.depth.counts <- lapply(list.o.depths,
+                           function(fileName) {
+                             
+                             metagenomeID = fileName %>%
+                               gsub("dataEdited/metabolic_analyses/depth/", "", .) %>%
+                               gsub("_depth.tsv", "", .)
+                             
+                             read.table(fileName,
+                                        stringsAsFactors = FALSE,
+                                        header = FALSE,
+                                        col.names = c("scaffoldID", "depth")) %>%
+                               mutate(depth = round(depth * normalized.coverage.vector[metagenomeID], 4)) %>%
+                               mutate(read.origin = metagenomeID)
+                             
+                           })
+
+
+#### Combine data into dataframe ####
+mean.df <- do.call(rbind,
+                   raw.depth.counts) %>%
+  spread(key = read.origin, value = depth, fill = NA)
+
+
+#### Write out file
+write.csv(mean.df,
+          "dataEdited/metabolic_analyses/depth/metabolicProtein_depth_clean.csv",
+          row.names = FALSE)
