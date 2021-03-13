d2f88a04baafbe8639d58b782367217b615e5817
Benjamin D. Peterson <petersonben50@gmail.com>
2021-03-13 14:56:01 -0600

    Updated hgcA plots to use new function

--- code/hgcA_analysis/hgcA_abundance.R
+++ code/hgcA_analysis/hgcA_abundance.R
@@ -9,6 +9,7 @@ library(lubridate)
 library(patchwork)
 library(readxl)
 library(tidyverse)
+source("code/gene_plotting_functions.R")
 cb.translator <- readRDS("/Users/benjaminpeterson/Box/ancillary_science_stuff/colors/colorblind_friendly_colors_R/colorblind_friendly_colors.rds")
 cb.translator <- c(cb.translator, "gray50")
 names(cb.translator)[length(cb.translator)] <- "gray50"
@@ -25,72 +26,99 @@ hgcA.manual.taxonomy <- read_xlsx("dataEdited/hgcA_analysis/phylogeny/manual_tax
 color.vector <- cb.translator[hgcA.manual.taxonomy$colorsToUse]
 names(color.vector) <- hgcA.manual.taxonomy$seqID
 
+
 #### Read in data ####
 hgcA.data <- read.csv("dataEdited/hgcA_analysis/depth/hgcA_coverage.csv") %>%
   filter(rep) %>%
   left_join(tax.data)
 
 
-#### Function to generate plots ####
-RM.of.interest <- 286
-year.of.interest <- 2017
-hgcA.profiling <- function(data.to.use = hgcA.data,
-                           RM.of.interest,
-                           year.of.interest,
-                           legend.position.to.use = "default",
-                           xlim.to.use = c(80, 0)) {
-  graph.to.make <- data.to.use %>%
-    filter(year(date) == year.of.interest,
-           RM == RM.of.interest) %>%
-    ggplot(aes(x = depth,
-               y = coverage,
-               fill = manual_classification)) +
-    geom_bar(stat = "identity") +
-    scale_fill_manual(values = color.vector) +
-    xlim(xlim.to.use) +
-    coord_flip() +
-    theme_classic() +
-    labs(title = paste("hgcA coverage at ",
-                       RM.of.interest,
-                       " in ",
-                       year.of.interest,
-                       sep = ""))
-  
-  if (legend.position.to.use[1] != "default") {
-    graph.to.make <- graph.to.make +
-      theme(legend.position = legend.position.to.use)
-  }
-  
-  graph.to.make
-  
-}
-
-profile.286.2018 <- hgcA.profiling(RM.of.interest = 286,
-                                   year.of.interest = 2018,
-                                   legend.position.to.use = "none")
-profile.300.2018 <- hgcA.profiling(RM.of.interest = 300,
-                                   year.of.interest = 2018,
-                                   legend.position.to.use = c(0.7, 0.8))
-
-profile.286.2018 + profile.300.2018
-
-
-
-profile.286.2017 <- hgcA.profiling(RM.of.interest = 286,
-                                   year.of.interest = 2017,
-                                   legend.position.to.use = c(0.7, 0.6))
-profile.300.2017 <- hgcA.profiling(RM.of.interest = 300,
-                                   year.of.interest = 2017,
-                                   legend.position.to.use = "none")
-
+#### 2017 profiles ####
+profile.286.2017 <- plot.profile.of.gene.with.taxonomy(data.to.use = hgcA.data,
+                                                       RM.of.interest = 286,
+                                                       year.of.interest = 2017,
+                                                       legend.position.to.use = "none",
+                                                       coverage_limits = c(0, 10),
+                                                       taxonomy.column.name = "manual_classification",
+                                                       ylab.to.use = "hgcA gene coverage\n(per 100X SCG)",
+                                                       titleToUse = "RM286 in 2017")
+profile.300.2017 <- plot.profile.of.gene.with.taxonomy(data.to.use = hgcA.data,
+                                                       RM.of.interest = 300,
+                                                       year.of.interest = 2017,
+                                                       legend.position.to.use = c(0.7, 0.8),
+                                                       coverage_limits = c(0, 2.5),
+                                                       taxonomy.column.name = "manual_classification",
+                                                       ylab.to.use = "hgcA gene coverage\n(per 100X SCG)",
+                                                       titleToUse = "RM300 in 2017")
+pdf("results/hgcA_analysis/profiles/2017_profiles.pdf",
+    height = 4.5,
+    width = 6)
 profile.286.2017 + profile.300.2017
+dev.off()
+
+#### 2017 close-up profile of metalimnion at 286 ####
+profile.286.2017.closeup <- plot.profile.of.gene.with.taxonomy(data.to.use = hgcA.data,
+                                                       RM.of.interest = 286,
+                                                       year.of.interest = 2017,
+                                                       legend.position.to.use = "none",
+                                                       coverage_limits = c(0, 0.25),
+                                                       depth_limits = c(40, 18),
+                                                       taxonomy.column.name = "manual_classification",
+                                                       ylab.to.use = "hgcA gene coverage")
+pdf("results/hgcA_analysis/profiles/2017_profiles_286metalimnion.pdf",
+    height = 4,
+    width = 3)
+profile.286.2017.closeup
+dev.off()
 
 
-# Zoom in on metalimnion in 2017
-profile.286.2017 <- hgcA.profiling(data.to.use = hgcA.data %>%
-                                     filter(depth < 41),
-                                   RM.of.interest = "RM286",
-                                   year.of.interest = 2017,
-                                   legend.position.to.use = c(0.7, 0.8),
-                                   xlim.to.use = c(41, 0))
-profile.286.2017
+#### 2018 profiles ####
+profile.286.2018 <- plot.profile.of.gene.with.taxonomy(data.to.use = hgcA.data,
+                                                       RM.of.interest = 286,
+                                                       year.of.interest = 2018,
+                                                       legend.position.to.use = "none",
+                                                       coverage_limits = c(0, 1),
+                                                       taxonomy.column.name = "manual_classification",
+                                                       ylab.to.use = "hgcA gene coverage\n(per 100X SCG)",
+                                                       titleToUse = "RM286 in 2018")
+profile.300.2018 <- plot.profile.of.gene.with.taxonomy(data.to.use = hgcA.data,
+                                                       RM.of.interest = 300,
+                                                       year.of.interest = 2018,
+                                                       legend.position.to.use = c(0.6, 0.8),
+                                                       coverage_limits = c(0, 3.5),
+                                                       taxonomy.column.name = "manual_classification",
+                                                       ylab.to.use = "hgcA gene coverage\n(per 100X SCG)",
+                                                       titleToUse = "RM300 in 2018")
+
+pdf("results/hgcA_analysis/profiles/2018_profiles.pdf",
+    height = 4.5,
+    width = 6)
+profile.286.2018 + profile.300.2018
+dev.off()
+
+#### 2019 profiles ####
+profile.300.2019 <- plot.profile.of.gene.with.taxonomy(data.to.use = hgcA.data,
+                                                       RM.of.interest = 300,
+                                                       year.of.interest = 2019,
+                                                       legend.position.to.use = c(0.6, 0.8),
+                                                       depth_limits = c(60, 0),
+                                                       coverage_limits = c(0, 0.8),
+                                                       taxonomy.column.name = "manual_classification",
+                                                       ylab.to.use = "hgcA gene coverage\n(per 100X SCG)",
+                                                       titleToUse = "RM300 in 2019")
+profile.310.2019 <- plot.profile.of.gene.with.taxonomy(data.to.use = hgcA.data,
+                                                       RM.of.interest = 310,
+                                                       year.of.interest = 2019,
+                                                       legend.position.to.use = "none",
+                                                       depth_limits = c(60, 0),
+                                                       coverage_limits = c(0, 0.8),
+                                                       taxonomy.column.name = "manual_classification",
+                                                       ylab.to.use = "hgcA gene coverage\n(per 100X SCG)",
+                                                       titleToUse = "RM310 in 2019")
+
+pdf("results/hgcA_analysis/profiles/2019_profiles.pdf",
+    height = 4.5,
+    width = 6)
+profile.300.2019 + profile.310.2019
+dev.off()
+
