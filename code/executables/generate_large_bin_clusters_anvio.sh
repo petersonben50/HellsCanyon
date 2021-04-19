73a809eadc435aa8f9d0cce32169e7c8eb30d5e1
Benjamin D. Peterson <petersonben50@gmail.com>
2021-04-19 11:59:16 -0500

    minor

--- code/executables/generate_large_bin_clusters_anvio.sh
+++ code/executables/generate_large_bin_clusters_anvio.sh
@@ -12,7 +12,6 @@
 # Activate anvio environment
 ##########################
 cd $anvioFolder
-mkdir original_summaries
 
 
 ##########################
@@ -29,7 +28,6 @@ PYTHONPATH=''
 anvi-display-contigs-stats $assembly.db \
                             --report-as-text \
                             -o original_summaries/$assembly\_summary.txt
-
 eukBins=`awk -F '\t' -v euk="eukarya (Protista_83)" '$1 == euk { print $2 }' original_summaries/$assembly\_summary.txt`
 prokBins=`awk -F '\t' -v prok="bacteria (Bacteria_71)" '$1 == prok { print $2 }' original_summaries/$assembly\_summary.txt`
 archBins=`awk -F '\t' -v arch="archaea (Archaea_76)" '$1 == arch { print $2 }' original_summaries/$assembly\_summary.txt`
