c6294c9d8b373ea01cbdbd067080ce26a790dcc4
Benjamin D. Peterson <petersonben50@gmail.com>
2021-06-23 12:12:58 -0500

    Added regression line

--- code/manuscript_figures/main/MeHg_hgcA.R
+++ code/manuscript_figures/main/MeHg_hgcA.R
@@ -11,7 +11,9 @@ cb.translator <- readRDS("/Users/benjaminpeterson/Box/ancillary_science_stuff/co
 
 
 #### Read in data ####
-Hg.data <- read.csv("dataEdited/waterChemistry/Hg_data.csv")
+Hg.data <- read.csv("dataEdited/waterChemistry/Hg_data.csv") %>%
+  group_by(date, RM, depth, constituent) %>%
+  summarise(concentration = sum(concentration))
 hgcA.data <- read.csv("dataEdited/hgcA_analysis/depth/hgcA_coverage.csv")
 
 
@@ -32,7 +34,7 @@ rm(Hg.data,
    hgcA.data)
 
 
-#### Make Hg and hgcA depth plots ####
+#### Set up vectors for Hg and hgcA depth plots ####
 color.vector <- c(cb.translator["black"],
                   cb.translator["bluishgreen"])
 names(color.vector) <- c("hgcA_coverage", "FMHG")
@@ -42,16 +44,23 @@ names(labels.vector) <- c("hgcA_coverage", "FMHG")
 points.vector <- c(16, 17)
 names(points.vector) <- c("hgcA_coverage", "FMHG")
 
-depth.plots <- all.data %>%
+
+#### Prepare data for plotting ####
+MeHg.hgcA.data <- all.data %>%
   filter(year(date) %in% c(2017, 2018),
          RM %in% c(286, 300)) %>%
+  spread(key = constituent,
+         value = concentration) %>%
+  filter(!is.na(hgcA_coverage)) %>%
   gather(key = constituent,
          value = abundance,
-         -c(1:3, 8)) %>%
-  filter(!is.na(abundance),
-         constituent %in% names(color.vector)) %>%
+         -c(1:4)) %>%
+  filter(constituent %in% names(color.vector)) %>%
   mutate(date.site = paste(year(date), "-RM", RM,
-                           sep = "")) %>%
+                           sep = ""))
+
+#### Generate depth plots ####
+depth.plots <- MeHg.hgcA.data %>%
   ggplot(aes(y = abundance,
              x = depth,
              group = constituent,
@@ -75,22 +84,64 @@ depth.plots <- all.data %>%
   ylab("Concentration (ng/L)\nGene coverage (per 100X SCG coverage")
 
 
-#### Make MeHg vs hgcA scatterplot ####
+#### Set up vectors for MeHg vs hgcA scatterplot ####
 redox.color.vector <- c(cb.translator["bluishgreen"],
                         cb.translator["orange"],
                         cb.translator["blue"])
 names(redox.color.vector) <- c("oxic", "suboxic", "euxinic")
 redox.year.vector <- c(16, 17, 2)
 names(redox.year.vector) <- c("2017", "2018", "2019")
-hgcA.MeHg.scatterplot <- all.data %>%
+
+
+#### Set up data for scatterplot ####
+MeHg.hgcA.scatterplot.data <- all.data %>%
+  spread(key = constituent,
+         value = concentration) %>%
   filter(!is.na(hgcA_coverage)) %>%
+  gather(key = constituent,
+         value = abundance,
+         -c(1:4)) %>%
+  filter(constituent %in% names(color.vector)) %>%
+  mutate(date.site = paste(year(date), "-RM", RM,
+                           sep = "")) %>%
+  ungroup() %>%
+  spread(key = constituent,
+         value = abundance) %>%
   mutate(redoxClassification = fct_relevel(redoxClassification,
-                                           names(redox.color.vector))) %>%
+                                           names(redox.color.vector)))
+
+
+#### Set hgcA coverage at 20m at RM286 in 2018 approximately to DL of hgcA coverage ####
+# We have a couple of lines that are essentially non-detects for hgcA.
+# I think they actually are below the effective DL of our analysis. Due to the 
+# log transformation, this is skewing the data left, so we're going to remove
+# these samples.
+MeHg.hgcA.scatterplot.data <- MeHg.hgcA.scatterplot.data %>%
+  filter(hgcA_coverage > 0.001)
+
+
+#### Linear regression of points ####
+mehg.hgcA.model <- lm(log(FMHG, 10) ~ log(hgcA_coverage, 10),
+                      data = MeHg.hgcA.scatterplot.data)
+summary(mehg.hgcA.model)
+# Get p-value
+f <- summary(mehg.hgcA.model)$fstatistic
+p.value <- pf(f[1],f[2],f[3],lower.tail=F) %>% round(4)
+
+#### Generate scatterplot ####
+hgcA.MeHg.scatterplot <- MeHg.hgcA.scatterplot.data %>%
   ggplot(aes(x = log(hgcA_coverage, 10),
              y = log(FMHG, 10),
-             group = redoxClassification,
+             color = redoxClassification,
              shape = as.character(year(date)))) +
   geom_point(aes(color = redoxClassification)) +
+  geom_abline(slope = coef(mehg.hgcA.model)[[2]],
+              intercept = coef(mehg.hgcA.model)[[1]]) +
+  geom_label(x = 0.5, y = -0.5,
+            label = paste("Adjusted r2 = ", round(summary(mehg.hgcA.model)$adj.r.squared, 2), "\n",
+                          "p = ", p.value,
+                          sep = ""),
+            color = "black") +
   scale_color_manual(values = redox.color.vector,
                      name = "Redox status") +
   scale_shape_manual(values = redox.year.vector,
@@ -102,9 +153,10 @@ hgcA.MeHg.scatterplot <- all.data %>%
         axis.text.x = element_text(colour = "black"),
         axis.text.y = element_text(colour = "black"),
         legend.key = element_rect(fill = "transparent", colour = "black"))
+hgcA.MeHg.scatterplot
 
 
-
+#### Save out scatterplot ####
 pdf("results/manuscript_figures/MeHg_hgcA.pdf",
     width = 8,
     height = 5)
