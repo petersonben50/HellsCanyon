c6f687f240d4ee542c78d5aa77bc1beee4984e85
Benjamin D. Peterson <petersonben50@gmail.com>
2022-02-13 14:47:31 -0600

    Plotting function to use

--- code/plotting_functions.R
+++ code/plotting_functions.R
@@ -1,6 +1,61 @@
-
 #### code/gene_plotting_functions.R ####
 # Written by Benjamin D. Peterson
+
+
+
+#### Set redox plotting function for TEAP figure ####
+plot.DO.nitrate.Mn.sulfide.profile <- function(geochem.data.of.interest = geochem.data,
+                                               RM.of.interest,
+                                               date.of.interest,
+                                               depth.range.of.interest,
+                                               concentration.of.interest,
+                                               legend.location.of.interest,
+                                               scaling.vector.to.use = NULL) {
+  geochem.data.of.interest <- geochem.data.of.interest %>%
+    filter(RM == RM.of.interest,
+           date == as.Date(date.of.interest)) %>%
+    group_by(RM, depth, date, constituent) %>%
+    summarize(concentration = mean(concentration)) %>%
+    filter(constituent %in% names(color.vector))
+
+  if (!is.null(scaling.vector.to.use)) {
+    for (modification.position in 1:length(scaling.vector.to.use)) {
+      modifications.to.keep <- geochem.data.of.interest %>%
+        filter(constituent == names(scaling.vector.to.use)[modification.position]) %>%
+        mutate(concentration = concentration * scaling.vector.to.use[modification.position])
+      geochem.data.of.interest <- geochem.data.of.interest %>%
+        filter(constituent != names(scaling.vector.to.use)[modification.position]) %>%
+        rbind(modifications.to.keep)
+    }
+  }
+
+  geochem.data.of.interest %>%
+    ggplot(aes(x = depth,
+               y = concentration,
+               color = constituent,
+               linetype = constituent,
+               shape = constituent)) +
+    geom_point(aes(color = constituent),
+               size = 2) +
+    geom_line(aes(color = constituent)) +
+    scale_colour_manual(values = color.vector,
+                        labels = labels.vector) +
+    scale_shape_manual(values = points.vector,
+                       labels = labels.vector) +
+    scale_linetype_manual(values = line.vector,
+                          labels = labels.vector) +
+    coord_flip(xlim = depth.range.of.interest,
+               ylim = concentration.of.interest) +
+    ylab("Concentration (mg/L)") +
+    xlab("Depth (m)") +
+    theme_classic() +
+    theme(legend.position = legend.location.of.interest,
+          legend.title = element_blank(),
+          legend.key = element_rect(fill = "transparent", colour = "black"),
+          legend.key.size = unit(1.75, 'lines'),
+          axis.text.x = element_text(colour = "black"),
+          axis.text.y = element_text(colour = "black"))
+}
 
 
 
@@ -19,9 +74,9 @@ plot.profile.for.multiple.genes <- function(marker.depth.df,
                                             legend.position.to.use = "default",
                                             legend.title.to.use = element_blank(),
                                             titleToUse = NULL) {
- 
+
   marker.depth.df[, "gene.names.to.use.for.filtering"] <- marker.depth.df[, gene.name.column]
-  
+
   clean.coverage <- marker.depth.df %>%
     filter(gene.names.to.use.for.filtering %in% genesOfInterest) %>%
     filter(year(ymd(date)) == yearOfInterest) %>%
@@ -38,25 +93,26 @@ plot.profile.for.multiple.genes <- function(marker.depth.df,
     scale_x_reverse(limits = depth_limits)  +
     theme(axis.text.x = element_text(colour="black"),
           axis.text.y = element_text(colour="black"))
-  
+
   #### Add labels and title ####
   if (is.null(titleToUse)) {
     titleToUse = paste("Coverage of",
                        paste(genesOfInterest, collapse = ","))
-    
+
   }
   graph.of.interest <- graph.of.interest +
     labs(title = titleToUse,
          y = xlab.to.use,
          x = "Depth (m)")
-  
+
   #### Add colors if defined in the call ####
   if (!is.null(color.vector.to.use)) {
     graph.of.interest <- graph.of.interest +
       geom_line(aes(group = gene.names.to.use.for.filtering,
                     color = gene.names.to.use.for.filtering,
                     linetype = gene.names.to.use.for.filtering)) +
-      geom_point(aes(color = gene.names.to.use.for.filtering,
+      geom_point(aes(group = gene.names.to.use.for.filtering,
+                     color = gene.names.to.use.for.filtering,
                      shape = gene.names.to.use.for.filtering)) +
       scale_colour_manual(values=color.vector.to.use)
   } else {
@@ -65,12 +121,12 @@ plot.profile.for.multiple.genes <- function(marker.depth.df,
       geom_line(aes(group = gene.names.to.use.for.filtering,
                     linetype = gene.names.to.use.for.filtering))
   }
-  
+
   #### Add point shapes if defined in the call ####
   if (!is.null(point.vector.to.use)) {
     graph.of.interest <- graph.of.interest +
       scale_shape_manual(values = point.vector.to.use)
-  } 
+  }
   #### Constrain x-axis if defined in the call ####
   if (!is.null(coverage_limits)) {
     graph.of.interest <- graph.of.interest +
@@ -82,7 +138,7 @@ plot.profile.for.multiple.genes <- function(marker.depth.df,
       max()
     graph.of.interest <- graph.of.interest +
       scale_y_continuous(limits = c(0, max.coverage))
-    
+
   }
 
   if (show.mean.coverage == TRUE) {
@@ -91,7 +147,7 @@ plot.profile.for.multiple.genes <- function(marker.depth.df,
                    col = "black", fill = "red",
                    size = 3, shape = 24)
   }
-  
+
   #### Set up legend position ####
   if (legend.position.to.use[1] != "default") {
     graph.of.interest <- graph.of.interest +
@@ -99,9 +155,9 @@ plot.profile.for.multiple.genes <- function(marker.depth.df,
             legend.title = legend.title.to.use,
             legend.box.background = element_rect(colour = "black"))
   }
-  
+
   graph.of.interest + coord_flip()
-  
+
 }
 
 
@@ -127,7 +183,7 @@ plot.profile.of.gene.with.taxonomy <- function(data.to.use,
                                                ylab.to.use = "Gene coverage (per 100X SCG coverage)",
                                                titleToUse = element_blank()) {
   data.to.use[, "taxonomy"] <- data.to.use[, taxonomy.column.name]
-  
+
   graph.to.make <- data.to.use %>%
     filter(year(date) == year.of.interest,
            RM == RM.of.interest) %>%
@@ -138,7 +194,7 @@ plot.profile.of.gene.with.taxonomy <- function(data.to.use,
     scale_fill_manual(values = color.vector.to.use) +
     xlim(depth_limits)+
     theme_classic()
-  
+
   #### Constrain x-axis if defined in the call ####
   if (!is.null(coverage_limits)) {
     graph.to.make <- graph.to.make +
@@ -154,26 +210,118 @@ plot.profile.of.gene.with.taxonomy <- function(data.to.use,
       max()
     graph.to.make <- graph.to.make +
       scale_y_continuous(limits = c(0, max.coverage))
-    
+
   }
-  
+
   if (legend.position.to.use[1] != "default") {
     graph.to.make <- graph.to.make +
       theme(legend.position = legend.position.to.use,
             legend.title = element_blank())
   }
-  
+
   #### Add labels and title ####
   graph.to.make <- graph.to.make +
     labs(x = xlab.to.use,
          y = ylab.to.use,
          title = titleToUse)
-  
+
   graph.to.make <- graph.to.make +
     theme(axis.text.x = element_text(colour = "black"),
           axis.text.y = element_text(colour = "black"))
-  
+
   graph.to.make +
-    coord_flip() 
-  
+    coord_flip()
+
+}
+
+
+
+
+
+
+
+
+#### Define function to plot one gene with taxonomy ####
+# data.to.use dataframe must have column with date, RM,
+# depth of sample collection, coverage of gene, and
+# a column with a taxonomic identifier.
+plot.profile.of.gene.with.taxonomy.function.noDepth <- function(data.to.use = hgcA.data,
+                                                                RM.of.interest = 286,
+                                                                year.of.interest = 2017,
+                                                                legend.position.to.use = "default",
+                                                                coverage_limits = NULL,
+                                                                taxonomy.column.name = "manual_classification",
+                                                                color.vector.tax.to.use = color.vector.tax,
+                                                                ylab.to.use = "Gene coverage (per 100X SCG coverage)",
+                                                                xlab.to.use = "Depth (m)",
+                                                                titleToUse = element_blank()) {
+  data.to.use[, "taxonomy"] <- data.to.use[, taxonomy.column.name]
+
+  data.to.use <- data.to.use %>%
+    filter(year(date) == year.of.interest,
+           RM == RM.of.interest) %>%
+    mutate(depth = as.character(depth))
+
+
+  depth.order <- unique(data.to.use$depth)[order(as.numeric(unique(data.to.use$depth)),
+                                                 decreasing = TRUE)]
+  tax.order.by.function <- data.to.use %>%
+    arrange(predicted_metabolism) %>%
+    select(manual_classification) %>%
+    unlist(use.names = FALSE) %>%
+    unique() %>%
+    as.character()
+
+  data.to.use <- data.to.use %>%
+    mutate(depth = fct_relevel(depth,
+                               depth.order),
+           taxonomy = fct_relevel(taxonomy,
+                                  tax.order.by.function))
+  graph.to.make <- data.to.use %>%
+    ggplot(aes(x = depth,
+               y = coverage,
+               fill = taxonomy)) +
+    geom_bar(stat = "identity") +
+    scale_fill_manual(values = color.vector.tax.to.use) +
+    theme_classic()
+
+  #### Constrain x-axis if defined in the call ####
+  if (!is.null(coverage_limits)) {
+    graph.to.make <- graph.to.make +
+      scale_y_continuous(limits = coverage_limits,
+                         expand = c(0, 0))
+  } else {
+    max.coverage <- data.to.use %>%
+      filter(year(date) == year.of.interest,
+             RM == RM.of.interest) %>%
+      group_by(depth) %>%
+      summarise(coverage = sum(coverage)) %>%
+      select(coverage) %>%
+      unlist() %>%
+      max()
+    graph.to.make <- graph.to.make +
+      scale_y_continuous(limits = c(0, max.coverage),
+                         expand = c(0, 0))
+
+  }
+
+  if (legend.position.to.use[1] != "default") {
+    graph.to.make <- graph.to.make +
+      theme(legend.position = legend.position.to.use,
+            legend.title = element_blank())
+  }
+
+  #### Add labels and title ####
+  graph.to.make <- graph.to.make +
+    labs(x = xlab.to.use,
+         y = ylab.to.use,
+         title = titleToUse)
+
+  graph.to.make <- graph.to.make +
+    theme(axis.text.x = element_text(colour = "black"),
+          axis.text.y = element_text(colour = "black"))
+
+  graph.to.make +
+    coord_flip()
+
 }
