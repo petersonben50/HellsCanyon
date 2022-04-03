cd59def3dbffd7297310e52f244c08b01363df18
Benjamin D. Peterson <petersonben50@gmail.com>
2022-04-03 13:09:02 -0500

    metabolic SI figure

--- code/metabolic_analyses/TEAP_figure_SI.R
+++ code/metabolic_analyses/TEAP_figure_SI.R
@@ -25,71 +25,59 @@ geochem.data <- rbind(read.csv("dataEdited/waterChemistry/geochem_WC.csv") %>%
 
 
 #### Vectors for geochem ####
-color.vector <- c(cb.translator["black"],
-                  cb.translator["reddishpurple"],
+color.vector <- c(cb.translator["reddishpurple"],
                   cb.translator["orange"],
                   cb.translator["blue"])
-names(color.vector) <- c("DO", "f_no3_mg_n_per_l", "f_mn_mg_per_l", "f_inorganic_sulfide_mg_per_l")
-line.vector <- c(4, 1, 3, 2)
+names(color.vector) <- c("f_no3_mg_n_per_l", "f_mn_mg_per_l", "f_inorganic_sulfide_mg_per_l")
+line.vector <- c(1, 3, 2)
 names(line.vector) <- names(color.vector)
-labels.vector <- c("DO (mg/L)", "NO3 (mgN/L)", "Diss. Mn", "Sulfide")
+labels.vector <- c("NO3 (mgN/L)", "Diss. Mn", "Sulfide")
 names(labels.vector) <- names(color.vector)
-points.vector <- c(NA, 16, 17, 18)
+points.vector <- c(21, 24, 23)
 names(points.vector) <- names(color.vector)
-scaling.vector <- 1/3
-names(scaling.vector) <- "DO"
 
 
 #### Generate geochem profiles ####
-geochem.2017.286 <- plot.DO.nitrate.Mn.sulfide.profile(geochem.data.of.interest = geochem.data,
-                                                       RM.of.interest = 286,
-                                                       date.of.interest = "2017-09-25",
-                                                       depth.range.of.interest = c(75, 0),
-                                                       concentration.of.interest = c(0, 2),
-                                                       legend.location.of.interest = "none",
-                                                       scaling.vector.to.use = scaling.vector)
-geochem.2017.300 <- plot.DO.nitrate.Mn.sulfide.profile(geochem.data.of.interest = geochem.data,
-                                                       RM.of.interest = 300,
-                                                       date.of.interest = "2017-09-26",
-                                                       depth.range.of.interest = c(75, 0),
-                                                       concentration.of.interest = c(0, 2),
-                                                       legend.location.of.interest = "none",
-                                                       scaling.vector.to.use = scaling.vector)
-geochem.2018.286 <- plot.DO.nitrate.Mn.sulfide.profile(geochem.data.of.interest = geochem.data,
-                                                       RM.of.interest = 286,
-                                                       date.of.interest = "2018-09-24",
-                                                       depth.range.of.interest = c(75, 0),
-                                                       concentration.of.interest = c(0, 2),
-                                                       legend.location.of.interest = "none",
-                                                       scaling.vector.to.use = scaling.vector)
-geochem.2018.300 <- plot.DO.nitrate.Mn.sulfide.profile(geochem.data.of.interest = geochem.data,
-                                                       RM.of.interest = 300,
-                                                       date.of.interest = "2018-09-25",
-                                                       depth.range.of.interest = c(75, 0),
-                                                       concentration.of.interest = c(0, 2),
-                                                       legend.location.of.interest = "none",
-                                                       scaling.vector.to.use = scaling.vector)
-geochem.2019.300 <- plot.DO.nitrate.Mn.sulfide.profile(geochem.data.of.interest = geochem.data,
-                                                       RM.of.interest = 300,
-                                                       date.of.interest = "2019-07-25",
-                                                       depth.range.of.interest = c(60, 0),
-                                                       concentration.of.interest = c(0, 2),
-                                                       legend.location.of.interest = "none",
-                                                       scaling.vector.to.use = scaling.vector)
-geochem.2019.310 <- plot.DO.nitrate.Mn.sulfide.profile(geochem.data.of.interest = geochem.data,
-                                                       RM.of.interest = 310,
-                                                       date.of.interest = "2019-07-23",
-                                                       depth.range.of.interest = c(60, 0),
-                                                       concentration.of.interest = c(0, 2),
-                                                       legend.location.of.interest = "none",
-                                                       scaling.vector.to.use = scaling.vector)
-
-
-
-ggarrange(geochem.2017.286, geochem.2017.300,
-          geochem.2018.286, geochem.2018.300,
-          geochem.2019.300, geochem.2019.310,
-          ncol = 2, nrow = 3)
+geochem.2017.286 <- plot.redox.profile(geochem.data.of.interest = geochem.data,
+                                       RM.of.interest = 286,
+                                       date.of.interest = "2017-09-25",
+                                       depth.range.of.interest = c(75, 0),
+                                       concentration.of.interest = c(0, 2),
+                                       legend.location.of.interest = c(0.8, 0.5))
+geochem.2017.300 <- plot.redox.profile(geochem.data.of.interest = geochem.data,
+                                       RM.of.interest = 300,
+                                       date.of.interest = "2017-09-26",
+                                       depth.range.of.interest = c(75, 0),
+                                       concentration.of.interest = c(0, 2),
+                                       legend.location.of.interest = "none")
+geochem.2018.286 <- plot.redox.profile(geochem.data.of.interest = geochem.data,
+                                       RM.of.interest = 286,
+                                       date.of.interest = "2018-09-24",
+                                       depth.range.of.interest = c(75, 0),
+                                       concentration.of.interest = c(0, 2),
+                                       legend.location.of.interest = "none")
+geochem.2018.300 <- plot.redox.profile(geochem.data.of.interest = geochem.data,
+                                       RM.of.interest = 300,
+                                       date.of.interest = "2018-09-25",
+                                       depth.range.of.interest = c(75, 0),
+                                       concentration.of.interest = c(0, 2),
+                                       legend.location.of.interest = "none")
+geochem.2019.300 <- plot.redox.profile(geochem.data.of.interest = geochem.data,
+                                       RM.of.interest = 300,
+                                       date.of.interest = "2019-07-25",
+                                       depth.range.of.interest = c(60, 0),
+                                       concentration.of.interest = c(0, 2),
+                                       legend.location.of.interest = "none")
+geochem.2019.310 <- plot.redox.profile(geochem.data.of.interest = geochem.data,
+                                       RM.of.interest = 310,
+                                       date.of.interest = "2019-07-23",
+                                       depth.range.of.interest = c(60, 0),
+                                       concentration.of.interest = c(0, 2),
+                                       legend.location.of.interest = "none")
+# ggarrange(geochem.2017.286, geochem.2017.300,
+#           geochem.2018.286, geochem.2018.300,
+#           geochem.2019.300, geochem.2019.310,
+#           ncol = 2, nrow = 3)
 
 
 #### Nitrate reduction ####
@@ -97,7 +85,7 @@ color.vector <- c(cb.translator["vermillion"],
                   cb.translator["orange"],
                   cb.translator["reddishpurple"])
 names(color.vector) <- c("narG", "nirK", "nirS")
-N.max.coverage <- 150
+N.max.coverage <- 125
 nar.286.2017 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                                 genesOfInterest = names(color.vector),
                                                 yearOfInterest = "2017",
@@ -129,7 +117,7 @@ nar.286.2018 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                                 coverage_limits = c(0, N.max.coverage),
                                                 color.vector.to.use = color.vector,
                                                 xlab.to.use = "Gene abundance",
-                                                legend.position.to.use = c(0.8, 0.8))
+                                                legend.position.to.use = "none")
 
 nar.300.2018 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                                 genesOfInterest = names(color.vector),
@@ -140,7 +128,7 @@ nar.300.2018 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                                 coverage_limits = c(0, N.max.coverage),
                                                 color.vector.to.use = color.vector,
                                                 xlab.to.use = "Gene abundance",
-                                                legend.position.to.use = c(0.8, 0.8))
+                                                legend.position.to.use = "none")
 nar.300.2019 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                                 genesOfInterest = names(color.vector),
                                                 yearOfInterest = "2019",
@@ -150,7 +138,7 @@ nar.300.2019 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                                 coverage_limits = c(0, N.max.coverage),
                                                 color.vector.to.use = color.vector,
                                                 xlab.to.use = "Gene abundance",
-                                                legend.position.to.use = c(0.8, 0.8))
+                                                legend.position.to.use = "none")
 nar.310.2019 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                                 genesOfInterest = names(color.vector),
                                                 yearOfInterest = "2019",
@@ -160,7 +148,7 @@ nar.310.2019 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                                 coverage_limits = c(0, N.max.coverage),
                                                 color.vector.to.use = color.vector,
                                                 xlab.to.use = "Gene abundance",
-                                                legend.position.to.use = c(0.8, 0.8))
+                                                legend.position.to.use = "none")
 
 
 
@@ -168,7 +156,7 @@ nar.310.2019 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
 #### Sulfate cycling ####
 color.vector <- c(cb.translator["blue"], cb.translator["skyblue"])
 names(color.vector) <- c("dsrA", "mcrA")
-max.coverage <- 5
+max.coverage <- 4.5
 S.286.2017 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                               genesOfInterest = names(color.vector),
                                               yearOfInterest = "2017",
@@ -181,37 +169,37 @@ S.286.2017 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                               legend.position.to.use = c(0.8, 0.8))
 
 S.300.2017 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
-                                                genesOfInterest = names(color.vector),
-                                                yearOfInterest = "2017",
-                                                RMofInterest = "300",
-                                                show.mean.coverage = FALSE,
-                                                depth_limits = c(75, 0),
-                                                coverage_limits = c(0, max.coverage),
-                                                color.vector.to.use = color.vector,
-                                                xlab.to.use = "Gene abundance",
-                                                legend.position.to.use = c(0.8, 0.8))
+                                              genesOfInterest = names(color.vector),
+                                              yearOfInterest = "2017",
+                                              RMofInterest = "300",
+                                              show.mean.coverage = FALSE,
+                                              depth_limits = c(75, 0),
+                                              coverage_limits = c(0, max.coverage),
+                                              color.vector.to.use = color.vector,
+                                              xlab.to.use = "Gene abundance",
+                                              legend.position.to.use = c(0.8, 0.8))
 
 S.286.2018 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
-                                                genesOfInterest = names(color.vector),
-                                                yearOfInterest = "2018",
-                                                RMofInterest = "286",
-                                                show.mean.coverage = FALSE,
-                                                depth_limits = c(75, 0),
-                                                coverage_limits = c(0, max.coverage),
-                                                color.vector.to.use = color.vector,
-                                                xlab.to.use = "Gene abundance",
-                                                legend.position.to.use = c(0.8, 0.8))
+                                              genesOfInterest = names(color.vector),
+                                              yearOfInterest = "2018",
+                                              RMofInterest = "286",
+                                              show.mean.coverage = FALSE,
+                                              depth_limits = c(75, 0),
+                                              coverage_limits = c(0, max.coverage),
+                                              color.vector.to.use = color.vector,
+                                              xlab.to.use = "Gene abundance",
+                                              legend.position.to.use = "none")
 
 S.300.2018 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
-                                                genesOfInterest = names(color.vector),
-                                                yearOfInterest = "2018",
-                                                RMofInterest = "300",
-                                                show.mean.coverage = FALSE,
-                                                depth_limits = c(75, 0),
-                                                coverage_limits = c(0, max.coverage),
-                                                color.vector.to.use = color.vector,
-                                                xlab.to.use = "Gene abundance",
-                                                legend.position.to.use = c(0.8, 0.8))
+                                              genesOfInterest = names(color.vector),
+                                              yearOfInterest = "2018",
+                                              RMofInterest = "300",
+                                              show.mean.coverage = FALSE,
+                                              depth_limits = c(75, 0),
+                                              coverage_limits = c(0, max.coverage),
+                                              color.vector.to.use = color.vector,
+                                              xlab.to.use = "Gene abundance",
+                                              legend.position.to.use = "none")
 
 S.300.2019 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                               genesOfInterest = names(color.vector),
@@ -222,7 +210,7 @@ S.300.2019 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                               coverage_limits = c(0, max.coverage),
                                               color.vector.to.use = color.vector,
                                               xlab.to.use = "Gene abundance",
-                                              legend.position.to.use = c(0.8, 0.8))
+                                              legend.position.to.use = "none")
 
 S.310.2019 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                               genesOfInterest = names(color.vector),
@@ -233,7 +221,7 @@ S.310.2019 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
                                               coverage_limits = c(0, max.coverage),
                                               color.vector.to.use = color.vector,
                                               xlab.to.use = "Gene abundance",
-                                              legend.position.to.use = c(0.8, 0.8))
+                                              legend.position.to.use = "none")
 
 
 
@@ -241,15 +229,16 @@ S.310.2019 <- plot.profile.for.multiple.genes(marker.depth.df = gene.data,
 
 
 
-pdf("resu")
+pdf("results/manuscript_figures/TEAP_SI_figure.pdf",
+    width = 7.0,
+    height = 8)
 
 ggarrange(geochem.2017.286, nar.286.2017, S.286.2017,
           geochem.2017.300, nar.300.2017, S.300.2017,
           geochem.2018.286, nar.286.2018, S.286.2018,
           geochem.2018.300, nar.300.2018, S.300.2018,
-          # geochem.2019.300, nar.300.2019, S.300.2019,
-          # geochem.2019.310, nar.310.2019, S.310.2019,
-          # ncol = 6, nrow = 3,
-          ncol = 6, nrow = 2
+          geochem.2019.300, nar.300.2019, S.300.2019,
+          geochem.2019.310, nar.310.2019, S.310.2019,
+          ncol = 6, nrow = 3
           )
-
+dev.off()
