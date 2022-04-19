be3960956e3a5934d94b675d4db493bb4cb73f53
Benjamin D. Peterson <petersonben50@gmail.com>
2022-04-19 14:10:42 -0500

    Rewrote the set-up and starting with ORF prediction here.

--- code/binning/prolixibacteraceae_details/gene_neighborhoods/prolixibacteraceae_GN.sh
+++ code/binning/prolixibacteraceae_details/gene_neighborhoods/prolixibacteraceae_GN.sh
@@ -1,22 +1,66 @@
 #!/bin/sh
 
+############################################
+# code/prolixibacteraceae_details/gene_neighborhoods/prolixibacteraceae_GN.sh
+# Benjamin D. Peterson
 
+# This script will pull out the needed data
+# to do the gene neighborhood analyses on
+# hgcA.
+############################################
 
-
+if [ -f prolixibacteraceae_details ]; then
+  rm -rf prolixibacteraceae_details
+fi
 
 ############################################
+# Get set up
 ############################################
-# Genomic context for hgcA
-############################################
-############################################
-
-######################
-# First pull out hgcA+ scaffolds
-######################
-screen -S HCC_prolix_hgcA
+echo "LOADING UP FOLDERS AND NEEDED GENOMES"
 cd ~/HellsCanyon/dataEdited/binAnalysis
 mkdir prolixibacteraceae_details
-mkdir prolixibacteraceae_details/GN
+mkdir prolixibacteraceae_details/genomes
+
+original_prolix_analysis=/home/GLBRCORG/bpeterson26/HellsCanyon/dataEdited/binAnalysis/phylogeny/prolixibacteraceae
+grep '>' $original_prolix_analysis/hgcA_hits/hgcA_raw.faa | sed 's/>//' | while read geneID
+do
+  binID=`awk -F '\t' -v geneID="$geneID" '$1 == geneID { print $2 }' $original_prolix_analysis/ORFs_G2B.tsv`
+  cp $original_prolix_analysis/reference_bins/$binID.fna prolixibacteraceae_details/genomes
+done
+cp ~/HellsCanyon/dataEdited/binning/bins_hgcA_keepers/DNA/anvio_hgcA_0130.fna prolixibacteraceae_details/genomes
+cd prolixibacteraceae_details/genomes
+ls *fna | sed 's/.fna//' > ../genome_list.txt
+
+
+
+############################################
+# Predict ORFs
+############################################
+echo "PREDICTING ORFS WITH IMMA_ORF_STAN"
+
+cd ~/HellsCanyon/dataEdited/binAnalysis/prolixibacteraceae_details
+mkdir ORFs
+source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
+conda activate bioinformatics
+PYTHONPATH=''
+PERL5LIB=''
+HomeBio=/home/GLBRCORG/bpeterson26/HellsCanyon/code/homemade_bioinformatic_scripts
+
+cat genome_list.txt | while read accessionID
+do
+  bash $HomeBio/IMMA_ORF_STAN.sh -b $accessionID \
+                                -i genomes/$accessionID.fna \
+                                -o ORFs \
+                                -c $HomeBio/fasta_stuff/cleanFASTA.py
+done
+
+
+
+############################################
+# Pull out scaffolds of interest
+############################################
+cd ~/HellsCanyon/dataEdited/binAnalysis/prolixibacteraceae_details
+mkdir GN
 cd prolixibacteraceae_details/GN
 mkdir scaffolds
 rm -f scaffolds/reference_scaffolds*
