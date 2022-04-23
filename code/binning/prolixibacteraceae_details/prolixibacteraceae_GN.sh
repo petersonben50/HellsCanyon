1adc885ca452d89f216a1f778a34ee0e38dfbd8e
Benjamin D. Peterson <petersonben50@gmail.com>
2022-04-22 19:26:21 -0500

    Finalized gene neighborhood analysis

--- code/binning/prolixibacteraceae_details/prolixibacteraceae_GN.sh
+++ code/binning/prolixibacteraceae_details/prolixibacteraceae_GN.sh
@@ -9,23 +9,30 @@
 # hgcA.
 ############################################
 
+############################################
+# Prepare variables, directories, conda
+############################################
+cd /home/GLBRCORG/bpeterson26/HellsCanyon/dataEdited/binAnalysis
 if [ -f prolixibacteraceae_details ]; then
   rm -rf prolixibacteraceae_details
 fi
+mkdir prolixibacteraceae_details
+
 working_directory=/home/GLBRCORG/bpeterson26/HellsCanyon/dataEdited/binAnalysis/prolixibacteraceae_details
 original_prolix_analysis=/home/GLBRCORG/bpeterson26/HellsCanyon/dataEdited/binAnalysis/phylogeny/prolixibacteraceae
-HomeBio=/home/GLBRCORG/bpeterson26/HellsCanyon/code/homemade_bioinformatic_scripts
+HomeBio=/home/GLBRCORG/bpeterson26/HellsCanyon/code/HomeBio
 source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
 
+cd $working_directory
+mkdir reports/
+
+
 
 ############################################
-# Get set up
+# Retrieve needed genomes
 ############################################
 echo "LOADING UP FOLDERS AND NEEDED GENOMES"
-cd ~/HellsCanyon/dataEdited/binAnalysis
-
-mkdir prolixibacteraceae_details
-mkdir prolixibacteraceae_details/genomes
+mkdir genomes
 
 grep '>' $original_prolix_analysis/hgcA_hits/hgcA_raw.faa | sed 's/>//' | while read geneID
 do
@@ -38,23 +45,32 @@ ls *fna | sed 's/.fna//' > ../genome_list.txt
 
 
 
+
 ############################################
 # Predict ORFs
 ############################################
 echo "PREDICTING ORFS WITH IMMA_ORF_STAN"
-cd $working_directory
 mkdir ORFs
 conda activate bioinformatics
 PYTHONPATH=''
 PERL5LIB=''
 
+# Predict ORFs
 cat genome_list.txt | while read accessionID
 do
-  bash $HomeBio/IMMA_ORF_STAN.sh -b $accessionID \
-                                -i genomes/$accessionID.fna \
-                                -o ORFs \
-                                -c $HomeBio/fasta_stuff/cleanFASTA.py
-done
+  bash $HomeBio/PM4_binGeneration/IMMA_ORF_STAN_BINS.sh -b $accessionID \
+                                                        -i genomes/$accessionID.fna \
+                                                        -o ORFs \
+                                                        -c $HomeBio/fasta_manipulation/cleanFASTA.py
+done > reports/IMMA_ORF_STAN_log.txt
+
+# Generate scaffold to bin file
+bash $HomeBio/fasta_manipulation/Fasta_to_Scaffolds2Bin.sh -e faa \
+                                                           -i $working_directory/ORFs \
+                                                           > $working_directory/ORFs_G2B.tsv
+cat $working_directory/ORFs/*.faa > $working_directory/ORFs.faa
+conda deactivate
+
 
 
 
@@ -63,33 +79,98 @@ done
 ############################################
 echo "PULLING OUT GENE NEIGHBORHOOD INFORMATION FOR HGCA GENES"
 
-conda deactivate
 conda activate py_viz
 PYTHONPATH=''
 IFS=$'\n'
 mkdir $working_directory/GN_of_hgcA
-grep '>' $original_prolix_analysis/hgcA_hits/hgcA_raw.faa | sed 's/>//' > GN_of_hgcA/hgcA_id_list.txt
+grep '>' $original_prolix_analysis/hgcA_hits/hgcA_raw.faa | sed 's/>//' > $working_directory/GN_of_hgcA/hgcA_id_list.txt
 
-working_directory=/Users/benjaminpeterson/Documents/programs/homemade_bioinformatic_scripts/doctor_petersons_neighborhood/prolixibacteraceae_details
-HomeBio=/Users/benjaminpeterson/Documents/programs/homemade_bioinformatic_scripts/
-
-cd $working_directory
 cat GN_of_hgcA/hgcA_id_list.txt | while read orfFastaID
 do
   binID=`awk -F '\t' -v orfFastaID="$orfFastaID" '$1 == orfFastaID { print $2 }' ORFs_G2B.tsv`
-  python $HomeBio/doctor_petersons_neighborhood.py --bin_id $binID \
-                                                   --bin_file genomes/$binID.fna \
-                                                   --orf_fasta_id $orfFastaID \
-                                                   --ORF_location ORFs \
-                                                   --size_of_block 5000 \
-                                                   --outputLocation GN_of_hgcA
-done
+  python $HomeBio/AM3_binBasedAnalyses/doctor_petersons_neighborhood.py --bin_id $binID \
+                                                                         --bin_file genomes/$binID.fna \
+                                                                         --orf_fasta_id $orfFastaID \
+                                                                         --ORF_location ORFs \
+                                                                         --size_of_block 5000 \
+                                                                         --outputLocation GN_of_hgcA
+done > reports/doctor_petersons_neighborhood_log.txt
 
-
-
-
+# Concatenate the output files
 cd GN_of_hgcA
-# rm -f hgcA_geneNeighborhood.gff hgcA_geneNeighborhood.fna
+rm -f hgcA_geneNeighborhood_all_orfs.faa
 cat *_neighborhood.gff > hgcA_geneNeighborhood_all.gff
 cat *_neighborhood.fna > hgcA_geneNeighborhood_all.fna
+cat *.faa > hgcA_geneNeighborhood_all_orfs.faa
+rm -f *_[0-9]*.faa
 rm -f *_neighborhood.*
+conda deactivate
+
+
+############################################
+# Run KOFAMSCAN on the neighborhood ORFs
+############################################
+echo "PULLING OUT GENE NEIGHBORHOOD INFORMATION FOR HGCA GENES"
+conda activate kofamscan
+PYTHONPATH=""
+PERL5LIB=""
+analysis_name=prolix_hgcA_GN
+input_orfs=GN_of_hgcA/hgcA_geneNeighborhood_all_orfs.faa
+output_location=GN_of_hgcA/kofamscan_output
+cd $working_directory
+
+rm -f $output_location/$analysis_name\_raw.tsv
+echo "Running KOFAMscan for" $analysis_name
+~/references/kofamscan_files/kofam_scan-1.3.0/exec_annotation -f detail-tsv \
+                                                              -o $output_location/$analysis_name\_raw.tsv \
+                                                              $input_orfs
+
+
+
+############################################
+# Cluster ORFs with CD-HIT
+############################################
+source /home/GLBRCORG/bpeterson26/miniconda3/etc/profile.d/conda.sh
+conda activate bioinformatics
+PYTHONPATH=""
+PERL5LIB=""
+
+cd $working_directory
+mkdir GN_of_hgcA/clustering
+cd-hit -g 1 \
+        -i GN_of_hgcA/hgcA_geneNeighborhood_all_orfs.faa \
+        -o GN_of_hgcA/clustering/hgcA_geneNeighborhood_all_orfs_cluster_40.faa \
+        -c 0.40 \
+        -n 2 \
+        -d 0
+clstr2txt.pl GN_of_hgcA/clustering/hgcA_geneNeighborhood_all_orfs_cluster_40.faa.clstr \
+  > GN_of_hgcA/clustering/hgcA_geneNeighborhood_all_orfs_cluster_40.tsv
+
+
+# Files to download:
+# GN_of_hgcA/clustering/hgcA_geneNeighborhood_all_orfs_cluster_40.tsv
+# $output_location/$analysis_name\_quality_cleaned.tsv
+# $working_directory/GN_of_hgcA/hgcA_id_list.txt
+
+
+
+############################################
+# Generate figure
+############################################
+"""
+# On local machine
+conda activate py_viz
+PYTHONPATH=""
+PERL5LIB=""
+
+working_directory=/Users/benjaminpeterson/Documents/research/HellsCanyon/dataEdited/bins/binAnalysis/prolixibacteraceae_details
+HomeBio=/Users/benjaminpeterson/Documents/programs/HomeBio
+
+GFF_FILE = '/Users/benjaminpeterson/Documents/research/HellsCanyon/dataEdited/bins/binAnalysis/prolixibacteraceae_details/GN/hgcA_geneNeighborhood_all.gff'
+ORF_DATA = '/Users/benjaminpeterson/Documents/research/HellsCanyon/dataEdited/bins/binAnalysis/prolixibacteraceae_details/GN/hgcA_geneNeighborhood_info.txt'
+
+python $HomeBio/AM3_binBasedAnalyses/doctor_petersons_neighborhood_visualization.py --gff_file $working_directory/GN/hgcA_geneNeighborhood_all.gff \
+                                                                                    --orf_data $working_directory/GN/hgcA_geneNeighborhood_info.txt \
+                                                                                    --output_location $working_directory/GN/hgcA_geneNeighborhood_plot.pdf
+
+"""
