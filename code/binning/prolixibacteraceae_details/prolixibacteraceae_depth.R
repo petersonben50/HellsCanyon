4ae56ec1170811c4e3cae11f8e9a5f9526d344c1
Benjamin D. Peterson <petersonben50@gmail.com>
2022-04-22 21:48:54 -0500

    Added script to look at depth of prolixibacteraceae

---
+++ code/binning/prolixibacteraceae_details/prolixibacteraceae_depth.R
@@ -0,0 +1,46 @@
+#### code/binning/prolixibacteraceae_details/prolixibacteraceae_depth.R ####
+# Benjamin D. Peterson
+
+# This script will plot the abundance of all the
+# Prolixibacteraceae bins from this study.
+
+
+#### Always start with a clean slate ####
+rm(list = ls())
+setwd("/Users/benjaminpeterson/Documents/research/HellsCanyon/")
+library(lubridate)
+library(readxl)
+library(tidyverse)
+cb.translator <- readRDS("references/colorblind_friendly_colors.rds")
+cb.translator <- c(cb.translator, "grey50")
+names(cb.translator)[length(cb.translator)] <- "grey"
+
+
+#### Read in depth data ####
+depth.data <- readRDS("dataEdited/bins/binAnalysis/phylogeny/prolixibacteraceae/depth_of_bins.rds")
+
+
+#### Add in metadata ####
+MG.metadata <- read_xlsx("metadata/metagenome_metadata.xlsx")
+all.data <- depth.data%>%
+  left_join(MG.metadata)
+
+
+#### Set up color vector ####
+color.vector <- cb.translator
+names(color.vector) <- unique(all.data$HMS)
+
+
+#### Plot out data ####
+all.data %>%
+  filter(((RM %in% c(286, 300)) & (year(date) %in% c(2017, 2018))) |
+           ((RM %in% c(300, 310)) & (year(date) == 2019)))%>%
+  ggplot(aes(y = coverage,
+             x = depth,
+             group = HMS)) +
+  geom_point(aes(color = HMS)) +
+  geom_line(aes(color = HMS)) +
+  scale_color_manual(values = color.vector) +
+  facet_wrap(~year(date) + RM, nrow = 3) +
+  coord_flip(xlim = c(80, 0)) +
+  theme_bw()
