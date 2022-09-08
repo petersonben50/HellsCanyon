a6ef15cac214de113738df77597d7ab24360a741
Benjamin D. Peterson <petersonben50@gmail.com>
2022-09-08 16:58:22 -0700

    Updated seabird cleaning scripts. Included interpolation of data.

--- code/cleaning_scripts/seabird_data.R
+++ code/cleaning_scripts/seabird_data.R
@@ -9,38 +9,46 @@ library(readxl)
 library(tidyverse)
 
 
+#### Define river miles of interest ####
+RM.to.include <- c(286, 300, 310, 318)
+
 #### Read in data ####
-# 2017 and 2018 data
-sonde.data.2017.2018 <- read_xlsx("dataRaw/SeaCat/sondeProfiles_intensives2017-2018.xlsx") %>%
-  select(-c(`ChlA (EXO)`, `Turbidity (EXO)`, `FOM(EXO)`))
-names(sonde.data.2017.2018) <- c("date", "RM", "depth", "spec_cond", "redox_pot", "pH",
-                                 "temp", "DO", "ChlA", "turb", "PAR", "cDOM")
-sonde.data.2017.2018 <- sonde.data.2017.2018 %>%
-  mutate(date = as.Date(date)) %>%
-  filter(month(date) == 9)
-# 2019 data
-sonde.data.2019 <- read_xlsx("dataRaw/SeaCat/2019_intensive_seacat_brownlee.xlsx") %>%
-  select(-c(Datetime, Time, Location))
-names(sonde.data.2019) <- c("date", "depth", "temp", "pH", "redox_pot", "cDOM",
-                            "ChlA", "turb", "PAR", "spec_cond", "DO", "RM")
-sonde.data.2019 <- sonde.data.2019 %>%
-  filter(RM != "322") %>%
-  mutate(date = mdy(date)) %>%
-  select(c("date", "RM", "depth", "spec_cond", "redox_pot", "pH",
-           "temp", "DO", "ChlA", "turb", "PAR", "cDOM"))
+seacat.data <- read_xlsx("dataRaw/dataRelease_chem/v2/HCC - V2 Data Release_Master File_Oct 2019 to present_V2_9_forModelingGroup.xlsx",
+                         sheet = "Table_6_Brownlee_Profiles") %>%
+  rename(RM = snake_river_mile,
+         elevation_m = brownlee_reservoir_measurement_elevation_m) %>%
+  mutate(date = as.Date(measurement_collection_date_mm_dd_yy_h_mm)) %>%
+  filter(RM %in% RM.to.include) %>%
+  select(RM, date, elevation_m, diss_oxy_mg_per_l) %>%
+  filter(!is.na(diss_oxy_mg_per_l))
 
+unique.dates.sites <- unique(seacat.data[, c("RM", "date")]) %>%
+  as.data.frame()
+  
 
-#### Combine data ####
-sonde.data <- rbind(sonde.data.2017.2018,
-                    sonde.data.2019) %>%
-  gather(key = constituent,
-         value = concentration,
-         -c(1:3)) %>%
-  select(RM, date, depth, constituent, concentration)
-rm(sonde.data.2017.2018,
-   sonde.data.2019)
+for (entry in 1:dim(unique.dates.sites)[1]) {
+  seacat.data.temp <- seacat.data %>%
+    filter(RM == unique.dates.sites[entry, "RM"] &
+             date == unique.dates.sites[entry, "date"])
+  interpol.data.DO <- approx(x = seacat.data.temp$elevation_m,
+                             y = seacat.data.temp$diss_oxy_mg_per_l,
+                             xout = seq(floor(min(seacat.data.temp$elevation_m)),
+                                        ceiling(max(seacat.data.temp$elevation_m)),
+                                        1),
+                             rule = 2)
+  seacat.data.adjusted.temp <- data.frame(RM = unique.dates.sites[entry, "RM"],
+                                          date = unique.dates.sites[entry, "date"],
+                                          elevation_m = interpol.data.DO$x,
+                                          diss_oxy_mg_per_l = interpol.data.DO$y)
+  if (entry == 1) {
+    seacat.data.adjusted <- seacat.data.adjusted.temp
+  } else {
+    seacat.data.adjusted <- rbind(seacat.data.adjusted,
+                                  seacat.data.adjusted.temp)
+  }
+}
 
 
 #### Read out data ####
-saveRDS(sonde.data,
+saveRDS(seacat.data.adjusted,
         "dataEdited/seabird/seabird_data.rds")
