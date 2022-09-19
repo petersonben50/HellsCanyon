82bfcbc584c0f3d7873ddaae4315771c498e752b
Benjamin D. Peterson <petersonben50@gmail.com>
2022-09-19 16:45:26 -0700

    Cleaned up anoxia figure scripts

--- code/geochem/start_of_anoxia.R
+++ code/geochem/start_of_anoxia.R
@@ -2,7 +2,6 @@
 setwd("~/Documents/research/HellsCanyon/")
 library(fields)
 library(lubridate)
-library(plotly)
 library(readxl)
 library(tidyverse)
 cb.translator <- readRDS("references/colorblind_friendly_colors.rds")
@@ -27,24 +26,18 @@ min.do.data <- seabird.data %>%
   arrange(date, RM) %>%
   mutate(DO = DO + 0.001)
 
-profiling.dates.to.include <- min.do.data %>%
-  group_by(date) %>%
-  summarise(profiles_taken = n()) %>%
-  filter(profiles_taken >= 5) %>%
-  select(date) %>%
-  mutate(date = as.character(date)) %>%
-  unlist(use.names = FALSE)
- 
+
+#### Cap maximum DO (to highlight changes at lower concentrations) ####
+min.do.data$DO[which(min.do.data$DO > 10)] <- 10
+
 
 #### Function to generate contour plots of DO ####
-
 plot.anoxia.by.year <- function(year.of.interest) {
+
   min.do.data.year.of.interest <- min.do.data %>%
     filter(year(date) == year.of.interest) %>%
     spread(key = RM,
            value = DO)
-    
-
 
   min.do.data.matrix <- min.do.data.year.of.interest %>%
     select(-date) %>%
@@ -54,15 +47,46 @@ plot.anoxia.by.year <- function(year.of.interest) {
   min.do.data.matrix$y <- min.do.data.year.of.interest$date
   min.do.data.matrix$x <- as.numeric(colnames(min.do.data.year.of.interest)[-1])
 
+  # Calculate date and location of anoxia onset
+  date.of.anoxia <- min.do.data %>%
+    filter(year(date) == year.of.interest) %>%
+    arrange(date) %>%
+    filter(DO < 0.5) %>%
+    filter(date == min(date))
+  
+  # Generate plot
   filled.contour(x = min.do.data.matrix$x,
                  y = min.do.data.matrix$y,
                  z = min.do.data.matrix$z,
-                 ylim = c(as.Date(paste(year.of.interest, "-12-01",
+                 plot.axes={
+                   axis(1);
+                   axis(2,
+                        at = floor_date(seq(from = as.Date(paste(year.of.interest, "-04-01",
+                                                                 sep = "")),
+                                            to = as.Date(paste(year.of.interest, "-11-01",
+                                                               sep = "")),
+                                            by = 1),
+                                        'month') %>%
+                          unique(),
+                        labels = month(seq(from = as.Date(paste(year.of.interest, "-04-01",
+                                                                     sep = "")),
+                                                to = as.Date(paste(year.of.interest, "-11-01",
+                                                                   sep = "")),
+                                                by = 1),
+                                       label = TRUE,
+                                       abbr = TRUE) %>%
+                          unique());
+                   points(x = date.of.anoxia$RM,
+                          y = date.of.anoxia$date,
+                          col = cb.translator["reddishpurple"],
+                          pch = 16)},
+                 ylim = c(as.Date(paste(year.of.interest, "-11-01",
                                         sep = "")),
-                          as.Date(paste(year.of.interest, "-05-01",
+                          as.Date(paste(year.of.interest, "-04-01",
                                         sep = ""))),
-                 xlim = c(286, 330),
+                 xlim = c(286, 325),
                  zlim = c(0, 10),
+                 plot.title = title(main = year.of.interest),
                  nlevels = 40,
                  color.palette = colorRampPalette(c(cb.translator["black"], cb.translator["blue"], cb.translator["bluishgreen"], cb.translator["orange"]),
                                                   bias = 1, space = "rgb"))
@@ -72,13 +96,17 @@ plot.anoxia.by.year <- function(year.of.interest) {
 
 
 #### Save out PDF of plots ####
+par(mar = c(3, 3, 1, 1),
+    mgp=c(1.5,0.4,0),
+    tck=-0.008)
 pdf("results/geochem/start_of_anoxia.pdf",
     width = 6,
-    height = 6)
+    height = 5)
 plot.anoxia.by.year(2015)
 plot.anoxia.by.year(2016)
 plot.anoxia.by.year(2017)
 plot.anoxia.by.year(2018)
+plot.anoxia.by.year(2019)
 dev.off()
 
 
