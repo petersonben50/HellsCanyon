2944d69d87135bcb01c2978f831e330a53862f7d
Benjamin D. Peterson <petersonben50@gmail.com>
2022-10-28 16:14:25 -0700

    Updates for figure

--- code/hgcA_analysis/tree_final.R
+++ code/hgcA_analysis/tree_final.R
@@ -12,12 +12,13 @@ library(phangorn)
 library(readxl)
 library(tidyverse)
 library(treeio)
-cb.translator <- readRDS("~/Box/ancillary_science_stuff/colors/colorblind_friendly_colors_R/colorblind_friendly_colors.rds")
+cb.translator <- readRDS("references/colorblind_friendly_colors.rds")
+cb.translator <- c(cb.translator, "gray50")
+names(cb.translator)[length(cb.translator)] <- "gray50"
 
 
 #### Read in needed files ####
 hgcA.list <- readLines("dataEdited/hgcA_analysis/hgcA_rep_list.txt")
-colorblind.color.vector <- readRDS("~/Box/ancillary_science_stuff/colors/colorblind_friendly_colors_R/colorblind_friendly_colors.rds")
 
 
 #### Read in Hg-MATE seq info ####
@@ -43,7 +44,7 @@ jones.metadata <- read.table("references/jones_bin_names.tsv",
   full_join(read.table("references/jones_genes2bin.tsv",
                        sep = '\t',
                        col.names = c("geneID", "binID"))) %>%
-  mutate(binName = paste(gsub("Unclassified", "", binName),
+  mutate(binName = paste(gsub("Unclassified ", "", binName),
                          sep = ""))
 jones.renaming.vector <- jones.metadata$binName
 names(jones.renaming.vector) <- jones.metadata$geneID
@@ -57,9 +58,14 @@ names(hgcA.renaming.vector) <- hgcA.df$seqID
 
 
 #### Read in hgcA to bin info ####
-hgcA.to.bin <- read.table("dataEdited/binning/hgcA/hgcA_to_bin.tsv",
-                          sep = '\t',
-                          header = TRUE)
+hgcA.to.bin.manual <- read.table("dataEdited/bins/binning/manualBinning/hgcA/hgcA_to_bin.tsv",
+                                 sep = '\t',
+                                 header = TRUE)
+hgcA.to.bin.auto <- read.table("dataEdited/bins/binning/autoBinning/hgcA_to_bin.tsv",
+                               sep = '\t',
+                               header = TRUE)
+hgcA.to.bin <- rbind(hgcA.to.bin.manual,
+                     hgcA.to.bin.auto)
 hgcA.to.bin.vector <- hgcA.to.bin$binID
 names(hgcA.to.bin.vector) <- hgcA.to.bin$hgcA_ID
 binned.hgcA.index <- which(names(hgcA.renaming.vector) %in% names(hgcA.to.bin.vector))
@@ -80,28 +86,35 @@ hgcA.tree.unrooted$tip.label <- paste(hgcA.tree.unrooted$tip.label %>% strsplit(
                                       sep = "_")
 hgcA.tree.unrooted$tip.label <- gsub("_NA_NA", "", hgcA.tree.unrooted$tip.label)
 
-# Root tree
+#### Root tree ####
 hgcA.tree <- root(phy = hgcA.tree.unrooted,
                   outgroup = c("paralog_Thermosulfurimonas_dismutans",
                                "paralog_Candidatus_Omnitrophica"),
                   edgelabel = TRUE)
 
 
-# Get indices
-reference.indices.mate <- which(hgcA.tree$tip.label %in% names(MATE.renaming.vector))
+#### Get indices ####
+reference.indices.mate <- which(hgcA.tree$tip.label %in% grep("MATE_ISO", names(MATE.renaming.vector),
+                                                              value = TRUE))
+mag.indices.mate <- which(hgcA.tree$tip.label %in% grep("MATE_MAG", names(MATE.renaming.vector),
+                                                        value = TRUE))
 this.study.indices <- which(hgcA.tree$tip.label %in% hgcA.list)
 reference.indices.jones <- which(hgcA.tree$tip.label %in% names(jones.renaming.vector))
 
-# Set color vector
-color.vector <- rep(colorblind.color.vector["reddishpurple"], length(hgcA.tree$tip.label))
-color.vector[reference.indices.mate] <- colorblind.color.vector["black"]
-color.vector[this.study.indices] <- colorblind.color.vector["vermillion"]
-color.vector[reference.indices.jones] <- cb.translator["skyblue"]
 
-# Visualize tree
-pdf("results/hgcA_analysis/hgcA_tree_RAxML_rooted.pdf",
-    height = 18,
-    width = 10)
+#### Set color vector ####
+color.vector <- rep(cb.translator["skyblue"], length(hgcA.tree$tip.label))
+color.vector[reference.indices.mate] <- cb.translator["black"]
+color.vector[mag.indices.mate] <- cb.translator["gray50"]
+color.vector[this.study.indices] <- cb.translator["vermillion"]
+color.vector[reference.indices.jones] <- cb.translator["blue"]
+
+
+
+#### Visualize tree ####
+# pdf("results/hgcA_analysis/hgcA_tree_RAxML_rooted.pdf",
+#     height = 18,
+#     width = 10)
 ggtree(hgcA.tree, aes(x = 0,
                       xend = 9)) +
   geom_tiplab(size=2.5, align = TRUE,
@@ -109,10 +122,12 @@ ggtree(hgcA.tree, aes(x = 0,
   geom_nodelab(aes(x = branch),
                vjust = -.3,
                size = 2)
-dev.off()
+# dev.off()
 
-# Change names:
+
+#### Change names ####
 hgcA.tree$tip.label[reference.indices.mate] <- MATE.renaming.vector[hgcA.tree$tip.label[reference.indices.mate]]
+hgcA.tree$tip.label[mag.indices.mate] <- MATE.renaming.vector[hgcA.tree$tip.label[mag.indices.mate]]
 hgcA.tree$tip.label[this.study.indices] <- hgcA.renaming.vector[hgcA.tree$tip.label[this.study.indices]]
 hgcA.tree$tip.label[reference.indices.jones] <- jones.renaming.vector[hgcA.tree$tip.label[reference.indices.jones]]
 
@@ -133,32 +148,44 @@ hgcA.tree.rootless <- drop.tip(hgcA.tree,
                                grep("paralog", hgcA.tree$tip.label))
 
 
-reference.indices.mate <- which(hgcA.tree.rootless$tip.label %in% MATE.renaming.vector)
-reference.indices.jones <- which(hgcA.tree.rootless$tip.label %in% jones.renaming.vector)
+
+#### Get indices ####
+reference.indices.mate <- grep("MATE_ISO", hgcA.tree.rootless$tip.label)
+mag.indices.mate <- grep("MATE_MAG", hgcA.tree.rootless$tip.label)
 this.study.indices <- which(hgcA.tree.rootless$tip.label %in% hgcA.renaming.vector)
+reference.indices.jones <- which(hgcA.tree.rootless$tip.label %in% jones.renaming.vector)
+
 
 #### Set color vector ####
-color.vector.to.use <- rep(cb.translator["reddishpurple"], length(hgcA.tree.rootless$tip.label))
-color.vector.to.use[reference.indices.mate] <- cb.translator["vermillion"]
-color.vector.to.use[this.study.indices] <- cb.translator["black"]
-color.vector.to.use[reference.indices.jones] <- cb.translator["skyblue"]
+color.vector <- rep(cb.translator["skyblue"], length(hgcA.tree.rootless$tip.label))
+color.vector[reference.indices.mate] <- cb.translator["black"]
+color.vector[mag.indices.mate] <- cb.translator["gray50"]
+color.vector[this.study.indices] <- cb.translator["vermillion"]
+color.vector[reference.indices.jones] <- cb.translator["blue"]
+
+
+#### Change bootstrap values to asterisks ####
+high.bootstrap.index <- which(as.numeric(hgcA.tree.rootless$node.label) >= 50)
+low.bootstrap.index <- which(as.numeric(hgcA.tree.rootless$node.label) < 50)
+hgcA.tree.rootless$node.label[high.bootstrap.index] <- "*"
+hgcA.tree.rootless$node.label[low.bootstrap.index] <- ""
 
 
 
 
 #### Make image of tree without root ####
 pdf("results/hgcA_analysis/hgcA_tree_RAxML_rootRemoved.pdf",
-    height = 11,
-    width = 8.5)
+    height = 9,
+    width = 7)
 ggtree(hgcA.tree.rootless,
        aes(x = 0,
            xend = 7)) +
   geom_tiplab(size = 1.9,
               align = TRUE,
-              colour = color.vector.to.use) +
+              colour = color.vector) +
   geom_nodelab(aes(x = branch),
-               vjust = -.3,
-               size = 1.8) +
+               vjust = 0.2,
+               size = 3.5) +
   geom_treescale(x = 0.05,
                  y = 80,
                  width = 0.5)
